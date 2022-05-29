fb399321e834b6165702e6e855aede87f63d91e4
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-05-29 21:46:13 +0530

    added dashboard api and integrated it

--- gaugau-final/src/api/dashboard.js
+++ gaugau-final/src/api/dashboard.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+import { URL } from "./constants";
 // GET DASHBOARD DATA
+export const dashboardData = async () => {
+  try {
+    const response = await axios.get(`${URL}/dashboard`);
+    const data = await response.data;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- gaugau-final/src/pages/Dashboard/Dashboard.jsx
+++ gaugau-final/src/pages/Dashboard/Dashboard.jsx
@@ -1,27 +1,41 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { dashboardData as fetchData } from "../../api/dashboard";
 const Dashboard = () => {
+  const [dashboardData, setDashboardData] = useState([]);
+  useEffect(() => {
+    const fetchDataA = async () => {
+      const data = await fetchData();
+      setDashboardData(data);
+    };
+    fetchDataA();
+  }, []);
+  // ? CHECKING IF DATA IS FETCHED AND STORED IN STATE OR NOT
+  // console.log("FETCHED:", dashboardData);
   return (
-    <section className="w-full h-full bg-pink-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 auto-cols-max">
+    <section className="w-full h-1/2 bg-pink-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 auto-cols-max">
       <div className="flex flex-col w-full h-full justify-center items-center bg-sky-100">
         {/* CREATE FELPER OBJECT FOR DASHBOAD */}
-        <h1 className="text-3xl text-sky-500"> 01</h1>
+        <h1 className="text-3xl text-sky-500">
+          {dashboardData[0].clientCount}
+        </h1>
         <h1 className="text-lg">Total Clients</h1>
       </div>
-      <div className="flex w-full h-full justify-center items-center bg-rose-500">
-        02
+      {/* CREATE HELPER OBJECT FOR DASHBOAD */}
+
+      <div className="flex flex-col w-full h-full justify-center items-center bg-sky-100">
+        <h1 className="text-3xl text-sky-500">
+          {dashboardData[1].customerMessage}
+        </h1>
+        <h1 className="text-lg">Total Customers</h1>
       </div>
-      <div className="flex w-full h-full justify-center items-center bg-rose-500">
-        03
-      </div>
-      <div className="flex w-full h-full justify-center items-center bg-rose-500">
-        04
-      </div>
-      <div className="flex w-full h-full justify-center items-center bg-rose-500">
-        05
+
+      <div className="flex flex-col w-full h-full justify-center items-center bg-sky-100">
+        <h1 className="text-3xl text-sky-500">
+          {dashboardData[2].campaignCount}
+        </h1>
+        <h1 className="text-lg">Total Campaign</h1>
       </div>
     </section>
   );
 };
-
 export default Dashboard;
