ffd26efa50f10ff7c1a32e2aca6178fb6a24ac2d
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-05-31 16:13:18 +0530

    [BUG]

--- gaugau-final/src/pages/Campaigns/Tabs.jsx
+++ gaugau-final/src/pages/Campaigns/Tabs.jsx
@@ -3,7 +3,7 @@ import { Fragment, useState } from "react";
 import Input from "../../components/Input/Input";
 import { GoSettings } from "react-icons/go";
 import CloseButton from "../../assets/close.svg";
-
+import DatePicker from "../../components/DatePicker/DatePicker";
 const CampaignTabs = () => {
   const [settingsSidebarToggle, setSettingsSidebarToggle] = useState(false);
 
@@ -91,46 +91,72 @@ const CampaignTabs = () => {
             </div>
           </form>
         </Tab.Panel>
-        <Tab.Panel className="relative">
-          <h1>Here Datatable will come</h1>
-          <button
-            onClick={handleClick}
-            className="px-5 py-2 flex justify-center gap-2 border-black border "
-          >
-            <GoSettings size={20} />
-            <span>Filter</span>
-          </button>
+        <Tab.Panel>
+          <div>
+            <h1>Here Datatable will come</h1>
+            <button
+              onClick={handleClick}
+              className="px-5 py-2 flex justify-center gap-2 border-black border "
+            >
+              <GoSettings size={20} />
+              <span>Filter</span>
+            </button>
 
-          {/* right settings bar */}
-          <div className="absolute -right-10 -top-[5.5em] w-[30vw] h-[92vh] bg-white drop-shadow-lg px-5">
-            <img
-              className="absolute right-5 top-5"
-              src={CloseButton}
-              alt="close"
-              width={20}
-            />
-            <div className="relative">
-              <form className="mt-20">
-                <Input
-                  label="Client"
-                  id="campaign-input-01"
-                  type="text"
-                  name="client"
-                  placeholder={"Select"}
-                  onChangeProp={handleChange}
-                />
-                <Input
-                  label="Campaign Title"
-                  id="campaign-input-01"
-                  type="text"
-                  name="client"
-                  onChangeProp={handleChange}
-                />
+            {/* right settings bar */}
+            <div
+              className={`absolute ${
+                settingsSidebarToggle
+                  ? "translate-x-[0%]"
+                  : "translate-x-[100%]"
+              } right-0 top-[3.7em] w-[100%] md:w-[30vw] h-[92vh] bg-white drop-shadow-lg px-5 transform transition ease-in-out duration-300`}
+            >
+              <img
+                onClick={handleClick}
+                className="absolute right-5 top-5 cursor-pointer"
+                src={CloseButton}
+                alt="close"
+                width={20}
+              />
+              <div className="relative">
+                <form className="mt-20">
+                  <Input
+                    label="Client"
+                    id="campaign-input-01"
+                    type="text"
+                    name="client"
+                    placeholder={"Select"}
+                    onChangeProp={handleChange}
+                  />
+                  <Input
+                    label="Campaign Title"
+                    id="campaign-input-01"
+                    type="text"
+                    name="client"
+                    onChangeProp={handleChange}
+                  />
 
-                <div>
-                  
-                </div>
-              </form>
+                  <h1>Date filter comes here</h1>
+                  <DatePicker />
+
+                  <div>
+                    <div className="flex gap-5">
+                      <button
+                        onClick={handleClick}
+                        className="w-1/2 text-primaryColor bg-white shadow-md py-2 px-4"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        onClick={handleClick}
+                        type="submit"
+                        className="w-1/2 text-gray-50 bg-primaryColor py-2 px-4"
+                      >
+                        Apply
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </Tab.Panel>
